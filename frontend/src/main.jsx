e83da629e4e6102140a6f9df31c6a99504a76a51
Vaishnavi <vaishnaviskpl@gmail.com>
2026-06-15 01:25:28 +0530

    Updated Name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import { AdminProvider } from '../context/AdminContext.jsx'
 import { registerSW } from "virtual:pwa-register";
 registerSW({ immediate: true });
-export const server="http://localhost:5000"
+export const server="https://milkdelivery-ygq5.onrender.com"
 createRoot(document.getElementById('root')).render(
 <AdminProvider>
     <App />
